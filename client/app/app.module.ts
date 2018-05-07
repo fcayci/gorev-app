0c3546cd3527faf019f04570c4c4a88d7f4997b2
Furkan Cayci <furkanca02@gmail.com>
2018-05-07 03:38:22 +0300

    removed form-person component - moved everything to people component.

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -8,7 +8,6 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { DummyComponent } from './components/dummy/dummy.component';
 import { PeopleComponent } from './components/people/people.component';
-import { FormPersonComponent } from './components/form-person/form-person.component';
 
 // define the routes
 const ROUTES : Routes = [
@@ -32,8 +31,7 @@ const ROUTES : Routes = [
     AppComponent,
     HomeComponent,
     DummyComponent,
-    PeopleComponent,
-    FormPersonComponent
+    PeopleComponent
   ],
   imports: [
     BrowserModule,
--- client/app/components/people/people.component.ts
+++ client/app/components/people/people.component.ts
@@ -12,6 +12,9 @@ import { FormsModule } from '@angular/forms';
 export class PeopleComponent {
 
   people : Kisi[];
+  model : Kisi = {...};
+
+  submitted = false;
 
   constructor(private dataService:DataService){
     this.dataService.getPeople()
@@ -20,4 +23,47 @@ export class PeopleComponent {
       });
   }
 
+  addPerson() {
+
+    // TODO: remove this for production
+    console.log('[adder.component.ts] addPerson() called...');
+
+    var result = this.people.filter(p => p.fullname.toLowerCase() === this.model.fullname).length;
+    if (result == 0) {
+
+      // TODO: remove this for production
+      console.log('[adder.component.ts] Adding person');
+      this.dataService.addPerson(this.model)
+        .subscribe(res => {
+          this.people.push(res);
+        });
+    }
+  }
+
+  onSubmit() {
+    this.submitted = true;
+  }
+
+  // Fancy stuff for modal view inside people component
+  public visible = false;
+  public visibleAnimate = false;
+
+  public show(): void {
+    this.visible = true;
+    setTimeout(() => this.visibleAnimate = true, 100);
+  }
+
+  public hide(): void {
+    this.visibleAnimate = false;
+    setTimeout(() => this.visible = false, 300);
+  }
+
+  public onContainerClicked(event: MouseEvent): void {
+    if ((<HTMLElement>event.target).classList.contains('modal')) {
+      this.hide();
+    }
+  }
+
+  //TODO: remove this for production
+  //get diagnostic() { return JSON.stringify(this.model); }
 }
